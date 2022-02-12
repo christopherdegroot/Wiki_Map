4bd5d87e52228e8c62b840c8e756784bcb2690cf
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-02-11 17:53:20 -0800

    rename to users to schema for table recreation, add SQL code for table drop/recreation

---
+++ db/schema/01_schema.sql
@@ -0,0 +1,40 @@
+-- Drop Tables if existant
+DROP TABLE IF EXISTS users CASCADE;
+
+-- Recreate Tables
+----------------------------------------
+
+-- Recreate User Table
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+-- Recreate Maps Table
+CREATE TABLE maps (
+  id SERIAL PRIMARY KEY NOT NULL,
+  zoom INTEGER,
+  center_latitude INTEGER,
+  center_longitude INTEGER
+);
+
+-- Recreate Pins Table
+CREATE TABLE pins (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE,
+
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  category VARCHAR(255),
+  image_url VARCHAR(255)
+);
+
+-- Recreate Users_Maps Join Table
+CREATE TABLE users_maps (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE
+);

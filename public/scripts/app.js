2089c93d4a7099deda72a3e8d43af342f155e961
John Chia <john.chia0106@gmail.com>
2022-02-12 02:13:57 -0800

    create dashboard.js script for all non-app related functions, update css files and built basic structure for index.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,12 +4,6 @@ $(() => {
 });
 
 
-// CSS for TOGGLE SIDE NAVBAR
-const openNav = () => {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("content").style.marginLeft = "250px";
-};
-const closeNav = () => {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("content").style.marginLeft = "0";
-};
+
+
+

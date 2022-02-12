66c2defec8324833bda70a7ce829e2a48d2e3950
John Chia <john.chia0106@gmail.com>
2022-02-11 22:06:07 -0800

    Add JQuery finish function inside app.js, delete .env-example file and create true .env file with GOOGLE API key within, update index.ejs to contain dashboard html, update sass

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,15 @@
-// Client facing scripts here
+
+$(() => {
+  // Client facing scripts here
+});
+
+
+// CSS for TOGGLE SIDE NAVBAR
+const openNav = () => {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("content").style.marginLeft = "250px";
+};
+const closeNav = () => {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("content").style.marginLeft = "0";
+};

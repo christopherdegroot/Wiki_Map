644af0f14bbcfc90d249461c2cc3b9c101f0aee6
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-02-12 21:14:11 -0800

    add placeholder test data for connecting to db

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,2 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, username, password) VALUES ('Chris D', 'cdg', '123');

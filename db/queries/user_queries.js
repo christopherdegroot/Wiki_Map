e79bb10eb4cac86b15fb9eb44c1b2988f8104568
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-02-14 15:01:43 -0800

    add favouritesByUserId, getUserWithId

--- db/queries/user_queries.js
+++ db/queries/user_queries.js
@@ -16,6 +16,10 @@ exports.getUserWithId = getUserWithId;
 // get favourites for a user by their user id
 const favouritesByUserId = function(id) {
   return pool.query(`
+  SELECT *
+  FROM users
+  JOIN users_maps_favourites ON user_id = users.id
+  WHERE users.id = $1
   `,[id])
   .then((response) =>{
     if (response.rows[0].length === 0) {return null}

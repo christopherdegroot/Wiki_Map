022d7f3d1ad1e4e96e3262bd182c98f14f1c4048
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-02-14 11:15:13 -0800

    changed pins to markers

--- db/seeds/03_pins.sql
+++
@@ -1,3 +0,0 @@
--- INSERT INTO pins (user_id, map_id, title, category, image_url) VALUES ();
-INSERT INTO pins (user_id, map_id, title, category, image_url) VALUES (1, 1, 'Favourite Restaurants', 'Food & Drink', 'http://www.demo-link.com/this-link-does-not-exist');
-

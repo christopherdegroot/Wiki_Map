a1983485b2e19ee287d187aa18480fc9a9bf6272
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-02-14 15:01:10 -0800

    add queries for getFavMapByUserId, getMarkersByCategory, mapsOwnedByUserId

--- db/queries/map_queries.js
+++ db/queries/map_queries.js
@@ -4,7 +4,7 @@ const getMapByUserId = function(id) {
   return pool.query(`
   SELECT *
   FROM maps
-  JOIN users_maps ON map_id = maps.id
+  JOIN users_maps_ownership ON map_id = maps.id
   JOIN users ON user_id = users.id
   WHERE users.id = $1;
   `,[id])
@@ -22,7 +22,7 @@ const getMapByMapId = function(id) {
   return pool.query(`
   SELECT *
   FROM maps
-  JOIN users_maps ON map_id = maps.id
+  JOIN users_maps_ownership ON map_id = maps.id
   JOIN users ON user_id = users.id
   WHERE maps.id = $1;
   `,[id])
@@ -33,6 +33,42 @@ const getMapByMapId = function(id) {
 
 }
 exports.getMapByMapId = getMapByMapId;
+
+
+// Get maps by selected as favourite map for a user
+const getFavMapByUserId = function(id) {
+  return pool.query(`
+  SELECT *
+  FROM maps
+  JOIN users_maps_favourites ON favourite_map_id = maps.id
+  JOIN users ON user_id = users.id
+  WHERE users.id = $1;
+  `,[id])
+  .then((response) =>{
+    if (response.rows[0].length === 0) {return null}
+    else {return response.rows[0]}
+  });
+
+}
+exports.getFavMapByUserId = getFavMapByUserId;
+
+// Get maps by owned maps for a user
+const getFavMapByUserId = function(id) {
+  return pool.query(`
+  SELECT *
+  FROM maps
+  JOIN users_maps_ownership ON map_id = maps.id
+  JOIN users ON owner_user_id = users.id
+  WHERE users.id = $1;
+  `,[id])
+  .then((response) =>{
+    if (response.rows[0].length === 0) {return null}
+    else {return response.rows[0]}
+  });
+
+}
+exports.getFavMapByUserId = getFavMapByUserId;
+
 
 
 // get all markers that match a particular map ID
@@ -55,6 +91,9 @@ exports.getMarkersByMapId = getMarkersByMapId;
 // get all maps that match a particular category
 const getMapsByCategory = function(category) {
   return pool.query(`
+  SELECT *
+  FROM maps
+  WHERE maps.map_category = '$1';
   `,[category])
   .then((response) =>{
     if (response.rows[0].length === 0) {return null}
@@ -68,6 +107,10 @@ exports.getMapsByCategory = getMapsByCategory;
 // get all markers by a particular category
 const getMarkersByCategory = function(category) {
   return pool.query(`
+  SELECT *
+  FROM markers
+  JOIN maps ON map_id = maps.id
+  WHERE maps.map_category = 'Food & Drink';
   `,[category])
   .then((response) =>{
     if (response.rows[0].length === 0) {return null}

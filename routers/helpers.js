4141d12bf52d8fa58a0f8ccae322ddf9294e7455
Jordan <jordan.benson4@gmail.com>
2022-02-15 14:03:26 -0800

    Remove mapData function, not needed. Change userObj format.

--- routers/helpers.js
+++ routers/helpers.js
@@ -1,28 +1,13 @@
-// Takes result of user data query and returns an object to be handled later by profileData function 
+// Takes result of user data query and returns an object to be handled later by profileData function
 const userData = (user) => {
-  const { name, profile_picture_url, bio } = user;
-  const userObj = { 
-    user: {
-      name,
+  const { username, profile_picture_url, user_bio } = user;
+  const userObj = {
+      username,
       profile_picture_url,
-      bio
-    }
+      bio: user_bio
   };
   return userObj;
 };
-
-// Takes results of users' map data query and returns an object to be handled later by profileData function ***** Not used right now, waiting on more db test data
-const mapsData = (maps) => { 
-  const mapVars = {};
-  maps.forEach((map) => {
-    const { id, map_title, map_category } = map;
-    mapVars[id] = { 
-      title: map_title, 
-      category: map_category
-    };
-  });
-  return mapVars;
-}
 
 // Takes results of resolved promises and outputs an object to directly inject into ejs template
 const profileData = (values) => {
@@ -33,13 +18,12 @@ const profileData = (values) => {
     user,
     maps,
     favourites
-  }
+  };
   return profileVars;
 };
 
 
 module.exports = {
   userData,
-  mapsData,
   profileData,
 };

b3b2337603566ddd022f6f8087cd06fbf09a678a
John Chia <john.chia0106@gmail.com>
2022-02-15 06:08:36 -0800

    change wording and remove uneccessary css

--- public/scripts/interface.js
+++ public/scripts/interface.js
@@ -7,40 +7,8 @@ $(() => {
 
   // john's attempt at animating the sliding motion, jQuery.toggle is very glitchy
   $('.top-btn').click(function() {
-    if ($(this).val() === "closed") {
-      $(this).val("open");
-      setTimeout(() => {$('.marker-list').css('width', '95%')}, 50);
-      setTimeout(() => {$('.marker-list').css('width', '89%')}, 100);
-      setTimeout(() => {$('.marker-list').css('width', '83%')}, 150);
-      setTimeout(() => {$('.marker-list').css('width', '77%')}, 200);
-      setTimeout(() => {$('.marker-list').css('width', '71%')}, 250);
-      setTimeout(() => {$('.marker-list').css('width', '65%')}, 300);
-      setTimeout(() => {$('.marker-list').css('width', '59%')}, 350);
-      setTimeout(() => {$('.marker-list').css('width', '53%')}, 400);
-      setTimeout(() => {$('.marker-list').css('width', '47%')}, 450);
-      setTimeout(() => {$('.marker-list').css('width', '41%')}, 500);
-      setTimeout(() => {$('.marker-list').css('width', '35%')}, 550);
-      setTimeout(() => {$('.marker-list').css('width', '30%')}, 600);
-      $('.new-marker').toggle(600);
-    } else {
-      $(this).val("closed");
-      setTimeout(() => {$('.marker-list').css('width', '35%')}, 50);
-      setTimeout(() => {$('.marker-list').css('width', '41%')}, 100);
-      setTimeout(() => {$('.marker-list').css('width', '47%')}, 150);
-      setTimeout(() => {$('.marker-list').css('width', '53%')}, 200);
-      setTimeout(() => {$('.marker-list').css('width', '59%')}, 250);
-      setTimeout(() => {$('.marker-list').css('width', '65%')}, 300);
-      setTimeout(() => {$('.marker-list').css('width', '71%')}, 350);
-      setTimeout(() => {$('.marker-list').css('width', '77%')}, 400);
-      setTimeout(() => {$('.marker-list').css('width', '83%')}, 450);
-      setTimeout(() => {$('.marker-list').css('width', '89%')}, 500);
-      setTimeout(() => {$('.marker-list').css('width', '95%')}, 550);
-      setTimeout(() => {$('.marker-list').css('width', '100%')}, 600);
-      $('.new-marker').toggle(600);
-    }
-
+    $('.new-marker').toggle(600);
   });
-
 });
 
 // function to initialize the google maps api

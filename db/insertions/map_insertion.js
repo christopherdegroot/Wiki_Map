cd99d1ec62bf5788d4615e57e98b7ce7d9fdfe50
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-02-15 15:18:32 -0800

    add pool to parameters

--- db/insertions/map_insertion.js
+++ db/insertions/map_insertion.js
@@ -1,4 +1,4 @@
-const addMap =  function(map) {
+const addMap =  function(map, pool) {
   const values = [`${map.map_title}`, `${map.map_category}`, `${map.map_rating}`, `${map.map_description}`, `${map.zoom}`, `${map.map_center_latitude}`, `${map.map_center_longitude}`];
   return pool.query(`
     INSERT INTO maps (map_title, map_category, map_rating, map_description, zoom, map_center_latitude, map_center_longitude)
@@ -12,7 +12,7 @@ const addMap =  function(map) {
 }
 exports.addMap = addMap;
 
-const addMarker =  function(marker) {
+const addMarker =  function(marker, pool) {
   const values = [`${markers.user_id}`, `${markers.map_id}`, `${markers.marker_title}`, `${markers.description}`, `${markers.marker_category}`, `${markers.image_url}`, `${markers.marker_latitude}`, `${markers.marker_longitude}`];
   return pool.query(`
     INSERT INTO markers (user_id, map_id, marker_title, description, marker_category, image_url, marker_latitude, marker_longitude)

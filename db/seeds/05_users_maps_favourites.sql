dd705a1fac653b7b402965c37c4de4268ee2081a
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-02-15 13:43:59 -0800

    adding relationship between data, adding ownership and favourites to users 1-7

--- db/seeds/05_users_maps_favourites.sql
+++ db/seeds/05_users_maps_favourites.sql
@@ -2,3 +2,12 @@ INSERT INTO users_maps_favourites (user_id, favourite_map_id) VALUES (1, 1);
 INSERT INTO users_maps_favourites (user_id, favourite_map_id) VALUES (1, 2);
 INSERT INTO users_maps_favourites (user_id, favourite_map_id) VALUES (2, 1);
 INSERT INTO users_maps_favourites (user_id, favourite_map_id) VALUES (2, 2);
+INSERT INTO users_maps_favourites (user_id, favourite_map_id) VALUES (3, 1);
+INSERT INTO users_maps_favourites (user_id, favourite_map_id) VALUES (3, 2);
+INSERT INTO users_maps_favourites (user_id, favourite_map_id) VALUES (3, 3);
+INSERT INTO users_maps_favourites (user_id, favourite_map_id) VALUES (3, 4);
+INSERT INTO users_maps_favourites (user_id, favourite_map_id) VALUES (3, 5);
+INSERT INTO users_maps_favourites (user_id, favourite_map_id) VALUES (3, 6);
+INSERT INTO users_maps_favourites (user_id, favourite_map_id) VALUES (3, 7);
+
+

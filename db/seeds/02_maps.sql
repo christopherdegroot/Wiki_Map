1180efa83cc8931b5713dd2eb45480bb531068ba
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-02-14 19:05:31 -0800

    change input parameters due to schema change

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,7 +1,7 @@
-INSERT INTO maps (title, map_category, zoom, map_center_latitude, map_center_longitude) VALUES ('North Shore Ski Resorts', 'Places & Locations', 8, '49.387483', '-123.080629');
-INSERT INTO maps (title, map_category, zoom, map_center_latitude, map_center_longitude) VALUES ('My favourite breweries', 'Food & Drink', 6, '49.268745', '-123.101554');
-INSERT INTO maps (title, map_category, zoom, map_center_latitude, map_center_longitude) VALUES ('Shops near me', 'Shopping', 7, '43.24883362474397', '-80.11541415714025');
-INSERT INTO maps (title, map_category, zoom, map_center_latitude, map_center_longitude) VALUES ('My favourite fast food places', 'Food & Drink', 4, '43.34883362474397', '-80.41541415714025');
-INSERT INTO maps (title, map_category, zoom, map_center_latitude, map_center_longitude) VALUES ('My USA roadtrip', 'Travel', 21, '41.908803', '-116.341949');
-INSERT INTO maps (title, map_category, zoom, map_center_latitude, map_center_longitude) VALUES ('My trip to Greece', 'Travel', 12, '39.393288', '21.893005');
-INSERT INTO maps (title, map_category, zoom, map_center_latitude, map_center_longitude) VALUES ('Hiking spots around Vancouver', 'Places & Locations', 11, '43.24883362474397', '-80.11541415714025');
+INSERT INTO maps (map_title, map_category, zoom, map_center_latitude, map_center_longitude) VALUES ('North Shore Ski Resorts', 'Places & Locations', 8, '49.387483', '-123.080629');
+INSERT INTO maps (map_title, map_category, zoom, map_center_latitude, map_center_longitude) VALUES ('My favourite breweries', 'Food & Drink', 6, '49.268745', '-123.101554');
+INSERT INTO maps (map_title, map_category, zoom, map_center_latitude, map_center_longitude) VALUES ('Shops near me', 'Shopping', 7, '43.24883362474397', '-80.11541415714025');
+INSERT INTO maps (map_title, map_category, zoom, map_center_latitude, map_center_longitude) VALUES ('My favourite fast food places', 'Food & Drink', 4, '43.34883362474397', '-80.41541415714025');
+INSERT INTO maps (map_title, map_category, zoom, map_center_latitude, map_center_longitude) VALUES ('My USA roadtrip', 'Travel', 21, '41.908803', '-116.341949');
+INSERT INTO maps (map_title, map_category, zoom, map_center_latitude, map_center_longitude) VALUES ('My trip to Greece', 'Travel', 12, '39.393288', '21.893005');
+INSERT INTO maps (map_title, map_category, zoom, map_center_latitude, map_center_longitude) VALUES ('Hiking spots around Vancouver', 'Places & Locations', 11, '43.24883362474397', '-80.11541415714025');

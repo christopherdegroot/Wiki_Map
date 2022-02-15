f6aa407a43155eea4ec37c0c64a0308a0f940df7
Jordan <jordan.benson4@gmail.com>
2022-02-15 13:14:13 -0800

    Modify profile route with Promises and helper functions to inject ejs variables.

--- routers/profile-router.js
+++ routers/profile-router.js
@@ -1,70 +1,55 @@
-/*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into api/users,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
-
 const express = require('express');
 const router  = express.Router();
-// const profileQueries = require('../db/profile_queries')  // Requiring separate query file once it is created
-// const mapQueries = require('../db/map_queries')  // Requiring separate query file once it is created
+const { userData, profileData } = require('./helpers');
+const userQueries = require('../db/queries/user_queries');  // Requiring separate query file once it is created
+const mapQueries = require('../db/queries/map_queries');  // Requiring separate query file once it is created
 
 module.exports = (db) => {
   ///////// Routes requiring separate DB query Promise to be returned
   
-  // Route using DB query function that returns a Promise of single user object 
-  // router.get('/:id', (req, res) => {
-    // profileQueries.getProfileById()
-      // .then((user) => {
-        // res.json(user);
-      // })
-      // .catch((err) => {
-        // res
-          // .status(500)
-          // .json({ error: err.message});
-      // });
-
-  // Route using DB query function that returns a Promise of an array of all public user objects
-  // router.get('/', (req, res) => {
-    // profileQueries.getPublicProfiles()
-      // .then((users) => {
-        // res.json(users);
-      // })
-      // .catch((err) => {
-        // res
-          // .status(500)
-          // .json({ error: err.message});
-      // });
-
-  ////////////////// Routes used in interim until DB query Promises are made
-
-  // Route that renders profile page of a single user
+  // Route using DB query function that returns a Promise of single user object
   router.get('/:id', (req, res) => {
-    const queryParams = [req.params.id];
-    db.query(`SELECT * FROM users WHERE id = $1`, queryParams)
+    const id = req.params.id;
+
+    // User info query Promise
+    const userPromise = userQueries.getUserWithId(id, db)
       .then((user) => {
-        const name = user.rows[0]['name'];
-        const templateVars = { name };
-        res.render('profile', templateVars);
+        const userObj = userData(user);
+        return userObj;
+      })
+      .catch((err) => {
+        res
+        .status(500)
+        .json({ error: err.message});
+      });
+
+    // All user maps query Promise
+    const mapPromise = mapQueries.getMapsByUserId(id, db)
+      .then((maps) => {
+        return maps;
+      })
+      .catch((err) => {
+        res
+        .status(500)
+        .json({ error: err.message});
+      });
+
+    // All user favourite maps query Promise
+    const favouritePromise = userQueries.favouriteMapsByUserId(id, db)
+      .then((maps) => {
+        return maps;
       })
       .catch((err) => {
         res
           .status(500)
           .json({ error: err.message});
       });
-  });
-  
-  // Route that returns an array of all public users as JSON 
-  router.get('/', (req, res) => {
-    db.query(`SELECT * FROM users;`)  // 
-    .then(users => {
-      res.json(users.rows);
-    })
-    .catch(err => {
-      res
-      .status(500)
-      .json({ error: err.message });
+
+    // Promise method to take all the Promise data and feed into helper function to create templateVars
+    Promise.all([userPromise, mapPromise, favouritePromise])
+    .then((values) => {
+      const templateVars = profileData(values);
+      res.render('profile', templateVars);
     });
   });
 

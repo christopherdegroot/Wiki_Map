547449bd73bc23faadd8d948b02352a570f25a86
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-02-16 00:34:43 -0800

    add get markers description query and get map description query

--- db/queries/map_queries.js
+++ db/queries/map_queries.js
@@ -15,6 +15,23 @@ const getMapByMapId = function(id, pool) {
 }
 exports.getMapByMapId = getMapByMapId;
 
+// Get map descriptive data by a map ID
+const getMapDescByMapId = function(id, pool) {
+  return pool.query(`
+  SELECT map_title, map_category, map_rating, map_description
+  FROM maps
+  WHERE maps.id = $1;
+  `,[id])
+  .then((response) =>{
+    if (response.rows[0].length === 0) {return null}
+    else {return response.rows[0]}
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+}
+exports.getMapDescByMapId = getMapDescByMapId;
+
 
 // get all markers that match a particular map ID
 const getMarkersByMapId = function(id, pool) {
@@ -34,6 +51,25 @@ const getMarkersByMapId = function(id, pool) {
 
 }
 exports.getMarkersByMapId = getMarkersByMapId;
+
+// get all markers that match a particular map ID
+const getMarkersDescByMapId = function(id, pool) {
+  return pool.query(`
+  SELECT markers.marker_title, markers.image_url, markers.description, marker_latitude, marker_longitude
+  FROM markers
+  JOIN maps ON map_id = maps.id
+  WHERE maps.id = $1;
+  `,[id])
+  .then((response) =>{
+    if (response.rows[0].length === 0) {return null}
+    else {return response.rows}
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+}
+exports.getMarkersDescByMapId = getMarkersDescByMapId;
 
 
 // get all maps that match a particular category

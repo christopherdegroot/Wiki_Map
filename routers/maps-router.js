526ebc572006fb3cd71d249102fa37c440bb277c
Jordan <jordan.benson4@gmail.com>
2022-02-15 16:35:04 -0800

    Add routes for new and edit map pages.

--- routers/maps-router.js
+++ routers/maps-router.js
@@ -1,68 +1,64 @@
-/*
- * All routes for Maps are defined here
- * Since this file is loaded in server.js into /maps,
- *   these routes are mounted onto /maps
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
-
 const express = require('express');
 const router  = express.Router();
-// const mapQueries = require('../db/map_queries')    // Requiring separate query file once it is created
+const mapQueries = require('../db/queries/map_queries');
 
 module.exports = (db) => {
-  ///////// Routes requiring separate DB query Promise to be returned
-  
-  // Route using DB query function that returns a Promise of single map object 
-  // router.get('/:id', (req, res) => {
-    // mapQueries.getMapById()
-      // .then((map) => {
-        // res.json(map);
-      // })
-      // .catch((err) => {
-        // res
-          // .status(500)
-          // .json({ error: err.message});
-      // });
 
-  // Route using DB query function that returns a Promise of an array of all public map objects
-  // router.get('/', (req, res) => {
-    // mapQueries.getPublicMaps()
-      // .then((maps) => {
-        // res.json(maps);
-      // })
-      // .catch((err) => {
-        // res
-          // .status(500)
-          // .json({ error: err.message});
-      // });
-
-  ////////////////// Routes used in interim until DB query Promises are made
-
-  // Route that returns a single map as JSON 
   router.get('/:id', (req, res) => {
-    const queryParams = [req.params.id];
-    db.query(`SELECT * FROM maps WHERE id = $1`, queryParams)
+    const id = req.params.id;
+
+    mapQueries.getMapByMapId(id, db)
       .then((map) => {
-        res.json(map.rows);
+        res.render('index');
       })
       .catch((err) => {
         res
           .status(500)
           .json({ error: err.message});
-      });
-  });
-  
-  // Route that returns an array of all public maps as JSON 
-  router.get('/', (req, res) => {
-    db.query(`SELECT * FROM maps;`)  // 
-    .then(maps => {
-      res.json(maps.rows);  // 
-    })
-    .catch(err => {
-      res
-      .status(500)
-      .json({ error: err.message });
+      })
     });
-  });
+
+  router.get('/:id/new', (req, res) => {
+    const id = req.params.id;
+
+    mapQueries.getMapByMapId(id, db)
+      .then((map) => {
+        res.render('new-map');
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({ error: err.message});
+      })
+    });
+
+    router.get('/:id/edit', (req, res) => {
+      const id = req.params.id;
+  
+      mapQueries.getMapByMapId(id, db)
+        .then((map) => {
+          res.render('edit-map');
+        })
+        .catch((err) => {
+          res
+            .status(500)
+            .json({ error: err.message});
+        })
+      });
+
+  router.post('/:id/delete', (req, res) => {
+    const id = req.params.id;
+    
+    mapQueries.deleteMapByMapId(id, db)
+      .then((map) => {
+        res.redirect('/profile/1')
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({ error: err.message});
+      })
+    });
+
   return router;
 };

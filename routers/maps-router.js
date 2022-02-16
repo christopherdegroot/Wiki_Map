0bad54328a57681849f2ad07848aa2818b2587af
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-02-16 00:36:30 -0800

    add templateVars for edit-map view and index

--- routers/maps-router.js
+++ routers/maps-router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router  = express.Router();
 const mapQueries = require('../db/queries/map_queries');
+const userQueries = require('../db/queries/user_queries');
+const { mapData, userData, markerData, mapEditData } = require('./helpers');
 
 module.exports = (db) => {
 
@@ -18,17 +20,20 @@ module.exports = (db) => {
       })
     });
 
+    // Route using DB query functions that returns a promise
   router.get('/:id', (req, res) => {
     const id = req.params.id;
-    mapQueries.getMapByMapId(id, db)
+
+    mapQueries.getMapDescByMapId(id, db)
       .then((map) => {
-        res.render('index');
+        const templateVars = mapData(map);
+        res.render('index', templateVars);
       })
       .catch((err) => {
         res
           .status(500)
           .json({ error: err.message});
-      })
+      });
     });
 
 
@@ -36,15 +41,46 @@ module.exports = (db) => {
     router.get('/:id/edit', (req, res) => {
       const id = req.params.id;
 
-      mapQueries.getMapByMapId(id, db)
-        .then((map) => {
-          res.render('edit-map');
-        })
-        .catch((err) => {
-          res
-            .status(500)
-            .json({ error: err.message});
-        })
+      // User info query Promise
+      const userPromise = userQueries.getUserWithId(id, db)
+      .then((user) => {
+        const userObj = userData(user);
+        return userObj;
+      })
+      .catch((err) => {
+        res
+        .status(500)
+        .json({ error: err.message});
+      });
+
+      // map info promise
+      const mapPromise = mapQueries.getMapByMapId(id, db)
+      .then((maps) => maps)
+      .catch((err) => {
+        res
+        .status(500)
+        .json({ error: err.message});
+      });
+
+      // marker info promise
+      const markerPromise = mapQueries.getMarkersDescByMapId(id, db)
+      .then((markers) => {
+        const markerArr = [];
+        markers.forEach(element => markerArr.push(element));
+        return markerArr;
+      })
+      .catch((err) => {
+        res
+        .status(500)
+        .json({ error: err.message});
+      });
+
+        Promise.all([userPromise, mapPromise, markerPromise])
+        .then((values) => {
+          const templateVars = mapEditData(values);
+          res.render('edit-map', templateVars);
+          console.log('template vars from MAPS-ROUTER: ', templateVars)
+        });
       });
 
   router.post('/:id/delete', (req, res) => {

203178559cc3add63127819f230357d14d01c22f
John Chia <john.chia0106@gmail.com>
2022-02-17 13:58:02 -0800

    edit queries for markers

--- db/editions/marker_editions.js
+++ db/editions/marker_editions.js
@@ -1,14 +1,14 @@
 
 
 const editMarker = function(marker, pool) {
-  const values = [marker.map_title, marker.map_category, marker.map_description, marker.id];
-
+  const values = [marker.marker_title, marker.image_url, marker.marker_longitude, marker.marker_latitude, marker.description, '1'];
+  // hard-coded markers.id to be 1 for now
   return pool.query(`
     UPDATE markers
-    SET map_title = $1, map_category = $2, map_description = $3
-    WHERE maps.id = $4
+    SET marker_title = $1, image_url = $2, marker_longitude = $3, marker_latitude = $4, description = $5
+    WHERE markers.id = $6
     `, values)
-    .then(() => {console.log(`Successfully updated map ${map.id}`)})
+    .then(() => {console.log(`Successfully updated marker ${marker.id}`)})
     .catch((err) => {
       console.log(err.message);
     });

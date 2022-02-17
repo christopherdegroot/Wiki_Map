e4dbf70600445a8edc9a49801a59269e81af398f
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-02-16 20:16:07 -0800

    create ajax for map view (index.js) page

--- public/scripts/map_client.js
+++ public/scripts/map_client.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
+  console.log('map client is being called')
 
-  const createMapListElement = function (mapObj) {
+  const createPinListElement = function (PinObj) {
 
     const escape = function (str) {
       let div = document.createElement("div");
@@ -8,128 +9,38 @@ $(document).ready(function () {
       return div.innerHTML;
     };
 
-    const $mapListElements = $(`
-        <div class="map-article">
-            <header class="article-header">
-              <form type="GET" action="/maps/${escape(mapObj.map_id)}">
-                <button class="title-btn">${escape(mapObj.map_title)}</button>
-              </form>
-              <div class="article-category">
-                <p>${escape(mapObj.map_category)}</p>
-              </div>
-            </header>
-            <div class="article-body">
-              <p>${escape(mapObj.map_description)}</p>
-              <div class="upvote">
-                <p>Upvotes: ${escape(mapObj.map_rating)}</p>
-              </div>
-            </div>
-            <div class="article-footer">
-              <form>
-                <button class="favorite-btn">Favorite</button>
-              </form>
-              <form type="GET" action="/maps/:id/edit">
-                <button class="edit-btn">edit</button>
-              </form>
-              <form>
-                <button class="delete-btn">delete</button>
-              </form>
-            </div>
-          </div>
-          `);
-
-    return $mapListElements;
-  };
-
-  // gets user ID off the url accessing current page
-  let urlUserId = ''
-  if (window.location.href.slice(-1) === '?') {
-    urlUserId += window.location.href.slice(-2);
-  } else {urlUserId += window.location.href.slice(-1);}
-
-  toString(urlUserId);
-  let newUrlUserId = urlUserId.replace('?', '');
-
-  const renderMapList = () => {
-    $.ajax({
-      url: `/profile/${newUrlUserId}/owned`,
-      method: 'GET',
-    })
-      .then((data) => {
-        $('#maps-container').empty();
-        data.forEach((map) => {
-          const $map = createMapListElement(map);
-          $('#maps-container').prepend($map);
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  // favorite maps
-  const createFavoriteListElement = function(mapObj) {
-
-    const escape = function(str) {
-      let div = document.createElement("div");
-      div.appendChild(document.createTextNode(str));
-      return div.innerHTML;
-    };
-
-    let edit;
-
-    if (mapObj.favourite_map_id !== mapObj.user_id) {
-      edit = `<form type="GET" action="/maps/${escape(mapObj.favourite_map_id)}/edit">
-        <button type='submit' class="edit-btn">edit</button>
-      </form>`
-    } else {
-      edit = '';
-    };
-
-    const $favoriteListElements = $(`
-      <div class="map-article">
-        <header class="article-header">
-          <form type="GET" action="/maps/${escape(mapObj.favourite_map_id)}">
-            <button class="title-btn">${escape(mapObj.map_title)}</button>
-          </form>
-          <div class="article-category">
-            <p>${escape(mapObj.map_category)}</p>
-          </div>
-        </header>
-        <div class="article-body">
-          <p>${escape(mapObj.map_description)}</p>
-          <div class="upvote">
-            <p>Upvotes: ${escape(mapObj.map_rating)}</p>
-            <div>
-                Map Created By: ${escape(mapObj.owner_user_id)}
-            </div>
-          </div>
-        </div>
-        <div class="article-footer">
-          <form>
-            <button class="unfavorite-btn">Un-favorite</button>
-          </form>
-         ${edit}
-          <form>
-            <button class="delete-btn">delete</button>
-          </form>
-        </div>
-      </div>
+    const $pinListElements = $(`
+    <div class="list">
+    <ul>
+    <li><a href="${'link to pin'}">${escape(pinObj.marker_title)}</a></li>
+    </ul>
+  </div>
     `);
 
-    return $favoriteListElements;
+    return $pinListElements;
   };
 
-  const renderFavoriteList = () => {
+  // gets map ID off the url accessing current page
+  let urlMapId = ''
+  if (window.location.href.slice(-1) === '?') {
+    urlMapId += window.location.href.slice(-2);
+  } else {urlMapId += window.location.href.slice(-1);}
+
+  toString(urlMapId);
+  let newUrlMapId = urlMapId.replace('?', '');
+
+  console.log('I am being called')
+
+  const renderMarkerList = () => {
     $.ajax({
-      url: `/profile/${newUrlUserId}/favourites`,
+      url: `/markers/${newUrlMapId}/fetch`,
       method: 'GET',
     })
       .then((data) => {
-        $('#favourites-container').empty();
-        data.forEach((map) => {
-          const $map = createFavoriteListElement(map);
-          $('#favourites-container').prepend($map);
+        $('#list').empty();
+        data.forEach((marker) => {
+          const $marker = createPinListElement(marker);
+          $('#list').prepend($marker);
         });
       })
       .catch((err) => {
@@ -137,6 +48,5 @@ $(document).ready(function () {
       });
   };
 
-  renderMapList();
-  renderFavoriteList();
+  renderMarkerList();
 });

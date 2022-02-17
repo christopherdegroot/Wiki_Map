90829d2cb0baff88a6abcc7ae1075b03a6c7dd91
Jordan <jordan.benson4@gmail.com>
2022-02-16 18:33:06 -0800

    Create routes for handling marker data.

---
+++ routers/markers-router.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router  = express.Router();
+const mapQueries = require('../db/queries/map_queries');
+
+module.exports = (db) => {
+
+
+  router.get('/:id/fetch', (req, res) => {
+    const id = req.params.id
+    console.log('id:', id);
+    mapQueries.getMarkersByMapId(id, db)
+      .then((markers) => res.jsonp(markers));
+  })
+  
+  router.post('/new', (req, res) => {
+    const queryBody = req.body;
+    console.log('qb: ', queryBody);
+    const vars = [];
+    for (const item in queryBody) {
+      // console.log('item:', item);
+      vars.push(queryBody[item]);
+    }
+    console.log('vars: ', vars);
+
+    // db.query(`SELECT * FROM maps WHERE id = $1`, queryBody)
+    // .then((map) => {
+      // res.json(map.rows);
+      // const { id, zoom, center_latitude, center_longitude } = map;
+      // const templateVars = { map };
+      // res.render('new-map', templateVars)
+    // })
+    // .catch((err) => {
+      // res
+      // .status(500)
+      // .json({ error: err.message});
+    // });
+  });
+
+  return router;
+};

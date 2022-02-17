a46b06487447282b2e7cb808f2be62610ad1bab9
John Chia <john.chia0106@gmail.com>
2022-02-16 20:42:30 -0800

    add post route for maps/:id/edit

--- routers/maps-router.js
+++ routers/maps-router.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const mapQueries = require('../db/queries/map_queries');
 const userQueries = require('../db/queries/user_queries');
 const userInsertions = require('../db/insertions/user_insertion');
 const mapInsertions = require('../db/insertions/map_insertion');
 const mapDeletions = require('../db/deletions/map_deletions');
-const { mapData, userData, markerData, mapEditData } = require('./helpers');
+const mapEditions = require('../db/editions/map_editions');
+const { mapData, userData, markerData, mapEditData, mapUserData } = require('./helpers');
 
 
 module.exports = (db) => {
+
+  // ------------------------ GET ROUTES -------------------------- //
 
   router.get('/new', (req, res) => {
     const id = req.params.id;
@@ -20,19 +23,7 @@ module.exports = (db) => {
       .catch((err) => {
         res
           .status(500)
-          .json({ error: err.message});
-      });
-  });
-
-  // post route to create new map, then refirect to edit page of that newly created map
-  router.post('/new', (req, res) => {
-    const newMap = req.body;
-    mapInsertions.addMap(newMap, db)
-      .then((x) => {
-        res.redirect(`/maps/${x.id}/edit`);
-      })
-      .catch((err) => {
-        console.log(err.message);
+          .json({ error: err.message });
       });
   });
 
@@ -47,23 +38,23 @@ module.exports = (db) => {
       .catch((err) => {
         res
           .status(500)
-          .json({ error: err.message});
+          .json({ error: err.message });
       });
 
-      // User info query Promise
-      const userPromise = userQueries.getUserWithId(id, db)
+    // User info query Promise
+    const userPromise = userQueries.getUserWithId(id, db)
       .then((user) => {
         const userObj = userData(user);
         return userObj;
       })
       .catch((err) => {
         res
-        .status(500)
-        .json({ error: err.message});
+          .status(500)
+          .json({ error: err.message });
       });
 
 
-      Promise.all([mapPromise, userPromise])
+    Promise.all([mapPromise, userPromise])
       .then((values) => {
         const templateVars = mapUserData(values);
         res.render('index', templateVars);
@@ -73,88 +64,114 @@ module.exports = (db) => {
   });
 
 
+  router.get('/:id/edit', (req, res) => {
+    const id = req.params.id;
 
-    router.get('/:id/edit', (req, res) => {
-      const id = req.params.id;
-
-      // User info query Promise
-      const userPromise = userQueries.getUserWithId(id, db)
+    // User info query Promise
+    const userPromise = userQueries.getUserWithId(id, db)
       .then((user) => {
         const userObj = userData(user);
         return userObj;
       })
       .catch((err) => {
         res
-        .status(500)
-        .json({ error: err.message});
+          .status(500)
+          .json({ error: err.message });
       });
 
-      // map info promise
-      const mapPromise = mapQueries.getMapByMapId(id, db)
-      .then((maps) => {return maps})
+    // map info promise
+    const mapPromise = mapQueries.getMapByMapId(id, db)
+      .then((maps) => {
+        return maps;
+      })
       .catch((err) => {
         res
-        .status(500)
-        .json({ error: err.message});
+          .status(500)
+          .json({ error: err.message });
       });
 
-      // marker info promise
-      const markerPromise = mapQueries.getMarkersDescByMapId(id, db)
+    // marker info promise
+    const markerPromise = mapQueries.getMarkersDescByMapId(id, db)
       .then((markers) => {
         return markers;
       })
       .catch((err) => {
         res
-        .status(500)
-        .json({ error: err.message});
+          .status(500)
+          .json({ error: err.message });
       });
 
-        Promise.all([userPromise, mapPromise, markerPromise])
-        .then((values) => {
-          const templateVars = mapEditData(values);
-          res.render('edit-map', templateVars);
-        });
+    Promise.all([userPromise, mapPromise, markerPromise])
+      .then((values) => {
+        const templateVars = mapEditData(values);
+        res.render('edit-map', templateVars);
       });
+  });
+
+
+  // ------------------------- POST ROUTES ------------------------- //
+
+
+
+
+  // post route to create new map, then refirect to edit page of that newly created map
+  router.post('/new', (req, res) => {
+    const newMap = req.body;
+    mapInsertions.addMap(newMap, db)
+      .then((x) => {
+        res.redirect(`/maps/${x.id}/edit`);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
+
 
   router.post('/:id/add', (req, res) => {
     const id = [req.params.id];
-    // const id = req.body;
-    console.log('id:', id);
     userInsertions.addFavourite(id, db)
       .then((map) => console.log(map))
       .catch((err) => {
         res
           .status(500)
-          .json({ error: err.message});
-      })
-    });
+          .json({ error: err.message });
+      });
+  });
 
   router.post('/:id/remove', (req, res) => {
     const id = [req.params.id];
-    // const id = req.body;
-    console.log('id:', id);
     mapDeletions.removeFavouriteMap(id, db)
       .then((map) => console.log(map))
       .catch((err) => {
         res
           .status(500)
-          .json({ error: err.message});
-      })
-    });
-    
+          .json({ error: err.message });
+      });
+  });
+
   router.post('/:id/delete', (req, res) => {
     const id = req.params.id;
 
     mapDeletions.deleteMap(id, db)
-      .then((map) => {
+      .then(() => {
         res.redirect('/profile/1');
       })
       .catch((err) => {
         res
           .status(500)
-          .json({ error: err.message});
+          .json({ error: err.message });
       });
-    });
+  });
+
+  router.post('/:id/edit', (req, res) => {
+    const map = req.body;
+    const id = req.params.id;
+    map.id = id;
+    mapEditions.editMap(map, db)
+      .then(() => {
+        res.redirect(`/maps/${id}/edit`);
+      });
+  });
 
 
   return router;
